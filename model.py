79d35624f8540561ca53db38c2d78a685645504f
wangzhe0912 <wangzhe0912@tju.edu.cn>
2018-08-04 14:04:28 +0000

    3th, crop and flip

--- model.py
+++ model.py
@@ -26,16 +26,24 @@ for line in lines:
     source_path = line[0]
     filename = './data/' + source_path
     image = cv2.imread(filename)
+    # crop the image
+    image = image[55:135, :, :]
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
+    reverse_image = cv2.flip(image, 1)
+    # plt.imshow(reverse_image)
+    # plt.show()
+    images.append(reverse_image)
+    reverse_measurement = -1 * float(line[3])
+    measurements.append(reverse_measurement)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0, input_shape=(80, 320, 3)))
 
 
 model.add(Convolution2D(6, 5, 5, border_mode='same'))
@@ -58,6 +66,6 @@ model.compile(loss="mse", optimizer='adam')
 
 model.summary()
 
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=20)
 
 model.save("model.h5")
